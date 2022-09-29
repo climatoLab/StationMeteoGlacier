e456e0444206cf802463358f16c32079b44fc794
RaphiRichi <112006378+RaphiRichi@users.noreply.github.com>
2022-09-29 17:46:22 -0400

    Librarie carte SD
    Enregistre les données sur la carte SD dans un dossier existant ou créer le dossier si nécessaire.

---
+++ cpp/StationMeteoGlacier/lib/SDLibrary.h
@@ -0,0 +1,63 @@
+#ifdef DEBUG
+
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+#include "fonctionsSD.h"
+
+#endif
+
+
+
+String donneesSD = "";
+// Détermine le nom des valeurs dans le tableau
+String labelsDonneesSD = String("Date;Heure;Vin;Temperature;Humidite;Pression;Intensite lumineuse; CO2 \r\n");
+
+
+void init_SD() {
+  if (!SD.begin(15)) {
+    Serial.println("Échec de montage de la carte SD !");    //montage?
+    return;
+  }
+
+  //Lecture du type de carte SD
+  uint8_t cardType = SD.cardType();
+  if (cardType == CARD_NONE) {
+    Serial.println("Incapable de détecter le type de carte !");
+    return;
+  }
+  Serial.print("Type de carte SD : "); Serial.println(cardType);
+  //Initialisation de la carte SD
+  Serial.println("Initialisation de la carte SD ...");
+
+
+//  if (!SD.begin()) {
+//    Serial.println("Erreur - Incapable d'initialiser la carte SD !");
+//    return;  //Échec de l'initialisation
+//  }
+  Serial.println("Carte SD initialisée correctement.");
+  //Ouverture du fichier donnee.txt
+
+
+  //Création du fichier de donnée si inexistant
+  bool i = false;
+  while ( i == false) {
+
+    File file = SD.open("/donnee.txt");
+    //Si le fichier donnee.txt n'existe pas sur la carte SD
+    if (!file) {
+      Serial.println("Le fichier donnee.txt n'existe pas !");
+      Serial.println("Écriture du nom des colonnes dans le fichier donnee.txt ...");
+      //Écriture du nom de chaque colonne pour le "logging" des données
+
+      writeFile(SD, "/donnee.txt", labelsDonneesSD.c_str());
+      i = true;
+    }
+    else {
+      Serial.println("Le fichier donnee.txt existe déjà !");
+      //Ferme le fichier
+      i = true;
+      file.close();
+    }
+  }
+}
