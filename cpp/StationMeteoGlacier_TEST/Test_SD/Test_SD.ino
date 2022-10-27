66202572a70f9088bf48a700a91fc9207f912094
ApolloVL <vincentlavalleewrk@outlook.com>
2022-10-27 18:32:41 -0400

    Update Test_SD.ino
    Création d'un fichier test pour l'utilisation de la carte SD

--- cpp/StationMeteoGlacier_TEST/Test_SD/Test_SD.ino
+++ cpp/StationMeteoGlacier_TEST/Test_SD/Test_SD.ino
@@ -1,9 +1,19 @@
-void setup() {
-  // put your setup code here, to run once:
+#define DEBUG
+#include "SDLibrary.h"
 
+String path = "/donnees.txt";//Nom du fichier qui contiendras les fichiers.
+String labelData = "Hello World!";//première ligne inscrite lors de la l'initialisation de la carte sd. 
+
+void setup() {
+Serial.begin(115200);
+i2c_init_();
+init_SD(path,labelData);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  String msg = String(random(100));
+  Serial.println(msg);
+  sendData(msg,path);
+  delay(1000);
 
 }
