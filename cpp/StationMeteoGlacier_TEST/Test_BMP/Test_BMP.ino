a5d5b356cf99dd2d74da4c488134c51e187111fa
ApolloVL <vincentlavalleewrk@outlook.com>
2022-10-28 17:12:09 -0400

    BMP388 Fichier test
    Ajout de commentaire.

--- cpp/StationMeteoGlacier_TEST/Test_BMP/Test_BMP.ino
+++ cpp/StationMeteoGlacier_TEST/Test_BMP/Test_BMP.ino
@@ -1,31 +1,41 @@
-#define DEBUG
-#define TIME_TO_SLEEP 4
-#define uS_TO_S_FACTOR 1000000  
+/******************************************************************
+ * Nom: Fichier Test pour la Librairie créer du BMP388
+ * Créer par: Vincent Lavallée
+ * Description: Ce programme assure et sert de référence pour de
+ *              futurs utilisation de la librairies créer par les
+ *              stagiaires de recherche sur la station météo.
+ * 
+******************************************************************/
 
-#include "bmpLib.h"
+#define DEBUG //-> Definition exclusivement pour le débogage des modules individuels.
 
-int x = 0;
-String DATA;
+#define TIME_TO_SLEEP 4 //Temps pour le deep sleep (sec)
+#define uS_TO_S_FACTOR 1000000  //Convertion de us a sec (microsec x 1M = sec)
+
+#include "bmpLib.h" //Librarie développer pour le BMP388
+String DATA; //Encapsule toutes les données prises par le BMP388 sous format -String-
 
 void setup() {
   Serial.begin(115200);
-  i2c_init_();
-  BMP3XX_init();  
-    
+  i2c_init_(); //-> Initiation du I2C // fonction seulement disponible en mode DEBUG
+  BMP3XX_init(); // Initialisation du capteur
+
+// Encapsulation des données:
+// Format : "Nom : Valeur"
   DATA =
   "Température(C): " + String(bmpTemp()) + "\n" +
   "Pression(hPa): " + String(bmpPression()) + "\n" +
   "Altitude(m): " + String(bmpAltitude()) + "\n";
 
-  Serial.println(DATA);
+  Serial.println(DATA); //Envoie sur le port série
   
-  delay(5000);
+  delay(5000); //Delai de 5 sec.
 
-  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
-  esp_deep_sleep_start();
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR); // Définition du deep sleep
+  esp_deep_sleep_start(); // Entre en deep sleep
   
 }
 
 void loop() {
-
+//  boucle loop() n'à aucune utilitée en mode deep sleep.
 }
--- cpp/StationMeteoGlacier_TEST/Test_BMP/bmpLib.h
+++ cpp/StationMeteoGlacier_TEST/Test_BMP/bmpLib.h
@@ -1,18 +1,31 @@
-#ifdef DEBUG
-#include <Wire.h>
-#include <SPI.h>
+/******************************************************************
+ * Nom: bmpLib.h
+ * Créer par: Vincent Lavallée, Annabelle Lambert, Raphael Richard
+ * Description: Librarie créer par les stagiaires de recherche de
+ *              Station météo pour permettre une acquisition simple
+ *              des données du BMP388.
+ *              
+ * Type de données :
+ *                  - Température (C) // Non-Calibré
+ *                  - Pression (Pa)   // Non-Calibré
+ *                  - Altitude (m)    // Non-Calibré
+ * 
+******************************************************************/
+
+#ifdef DEBUG //-> Définition pour le DEBUG des modules individuels, NE PAS ACTIVER DANS LE CODE COMPLET DE LA STATION.
+#include <Wire.h> //Introduit la librarie I2C
 #endif
 
+//--- LIBRARIES UTILISÉES POUR LE BMP 388-- //
+#include <Adafruit_Sensor.h> // DE ADAFRUIT //
+#include "Adafruit_BMP3XX.h" // DE ADAFRUIT //
+//---------------------------//-------------//
+#define SEALEVELPRESSURE_HPA (1013.25) //Définition de la pression au niveau de la mer.
 
-#include <Adafruit_Sensor.h>
-#include "Adafruit_BMP3XX.h"
+Adafruit_BMP3XX bmp; //Déclaration d'objet.
 
-#define SEALEVELPRESSURE_HPA (1013.25)
-
-Adafruit_BMP3XX bmp;
-
-bool BMP3XX_init(){
-  bool retCode = false;
+bool BMP3XX_init(){ // -> Fonction à introduire après l'initialisation du I2C. Permet l'initialisation de l'objet / module BMP388
+  bool retCode = false; // -> valeur de retour de référence.
   
   while (!Serial);
   Serial.println("Adafruit BMP388 / BMP390 test");
@@ -28,29 +41,37 @@ bool BMP3XX_init(){
     bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
     bmp.setOutputDataRate(BMP3_ODR_50_HZ);
   
-    retCode = true;
+    retCode = true; // -> true : confirme le fonctionnement du BMP388
   }
-  int temp = bmp.readTemperature();
-  int alt = bmp.readAltitude(SEALEVELPRESSURE_HPA);
-  int pressure = bmp.readPressure();
-
+  //------------------------------------------------//
+  // Prise de mesure initiale pour éviter la prise  // 
+  // de données erroné.                             //
+  //                                                //
+  // Remarque : Sans cette prise de donnée initiale //
+  //            le capteur ne fait que répéter les  //
+  //            mêmes données.                      //
+  //------------------------------------------------//
+  int temp = bmp.readTemperature();                 //
+  int alt = bmp.readAltitude(SEALEVELPRESSURE_HPA); //
+  int pressure = bmp.readPressure();                //
+  //------------------------------------------------//
   
-  return retCode;
+  return retCode; // -> retourne la valeur true si le capteur est bien initié, faux dans le cas contraire.
 }
 
-float bmpTemp(){
+float bmpTemp(){ // Retourne la température capté
   return bmp.readTemperature();
 }
 
-float bmpPression(int echelle = 100){
+float bmpPression(int echelle = 100){ // Retourne la pression capté
   return bmp.readPressure()/echelle;
 }
 
-float bmpAltitude(){
+float bmpAltitude(){ // Retourne l'alitude capté
   return bmp.readAltitude(SEALEVELPRESSURE_HPA);
 }
 
-String Err_bmp3xx(){
+String Err_bmp3xx(){ // Message d'erreur.
   if (! bmp.performReading()) {
     return "Failed to perform reading :(";
   }
@@ -59,11 +80,11 @@ String Err_bmp3xx(){
   }
 }
 
-uint8_t BMP3XX_ChipID(){
+uint8_t BMP3XX_ChipID(){ // Informations sur le BMP280;
   return bmp.chipID();
 }
 
-#ifdef DEBUG
+#ifdef DEBUG // -> initialisation de l'I2C, EN MODE DEBUG SEULEMENT
 void i2c_init_(const int PIN_GPS_EN = 12,const int PIN_POW_EN = 13){
 
   pinMode(PIN_GPS_EN, OUTPUT);
