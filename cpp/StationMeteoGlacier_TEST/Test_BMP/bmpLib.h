65b843bcb40840f067be1364b574f7616fdbb501
ApolloVL <vincentlavalleewrk@outlook.com>
2022-10-31 13:41:23 -0400

    DS3231
    Commentaire de librairie rtc et modification de la librairie BMP

--- cpp/StationMeteoGlacier_TEST/Test_BMP/bmpLib.h
+++ cpp/StationMeteoGlacier_TEST/Test_BMP/bmpLib.h
@@ -1,19 +1,19 @@
 /******************************************************************
- * Nom: bmpLib.h
- * Créer par: Vincent Lavallée, Annabelle Lambert, Raphael Richard
- * Description: Librarie créer par les stagiaires de recherche de
- *              Station météo pour permettre une acquisition simple
- *              des données du BMP388.
- *              
- * Type de données :
- *                  - Température (C) // Non-Calibré
- *                  - Pression (Pa)   // Non-Calibré
- *                  - Altitude (m)    // Non-Calibré
- *         
- * Liens : 
- *        -
- *        -
- * 
+   Nom: bmpLib.h
+   Créer par: Vincent Lavallée, Annabelle Lambert, Raphael Richard
+   Description: Librarie créer par les stagiaires de recherche de
+                Station météo pour permettre une acquisition simple
+                des données du BMP388.
+
+   Type de données :
+                    - Température (C) // Non-Calibré
+                    - Pression (Pa)   // Non-Calibré
+                    - Altitude (m)    // Non-Calibré
+
+   Liens :
+          -
+          -
+
 ******************************************************************/
 
 #ifdef DEBUG //-> Définition pour le DEBUG des modules individuels, NE PAS ACTIVER DANS LE CODE COMPLET DE LA STATION.
@@ -28,15 +28,15 @@
 
 Adafruit_BMP3XX bmp; //Déclaration d'objet.
 
-bool BMP3XX_init(){ // -> Fonction à introduire après l'initialisation du I2C. Permet l'initialisation de l'objet / module BMP388
+bool BMP3XX_init() { // -> Fonction à introduire après l'initialisation du I2C. Permet l'initialisation de l'objet / module BMP388
   bool retCode = false; // -> valeur de retour de référence.
-  
+
   while (!Serial);
   Serial.println("Adafruit BMP388 / BMP390 test");
 
   if (!bmp.begin_I2C()) {   // hardware I2C mode, can pass in address & alt Wire
-  //if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode  
-  //if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
+    //if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode
+    //if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
     Serial.println("Could not find a valid BMP3 sensor, check wiring!");
   } else {
     // Set up oversampling and filter initialization
@@ -44,11 +44,11 @@ bool BMP3XX_init(){ // -> Fonction à introduire après l'initialisation du I2C.
     bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
     bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
     bmp.setOutputDataRate(BMP3_ODR_50_HZ);
-  
+
     retCode = true; // -> true : confirme le fonctionnement du BMP388
   }
   //------------------------------------------------//
-  // Prise de mesure initiale pour éviter la prise  // 
+  // Prise de mesure initiale pour éviter la prise  //
   // de données erroné.                             //
   //                                                //
   // Remarque : Sans cette prise de donnée initiale //
@@ -59,37 +59,37 @@ bool BMP3XX_init(){ // -> Fonction à introduire après l'initialisation du I2C.
   int alt = bmp.readAltitude(SEALEVELPRESSURE_HPA); //
   int pressure = bmp.readPressure();                //
   //------------------------------------------------//
-  
+
   return retCode; // -> retourne la valeur true si le capteur est bien initié, faux dans le cas contraire.
 }
 
-float bmpTemp(){ // Retourne la température capté
+float bmpTemp() { // Retourne la température capté
   return bmp.readTemperature();
 }
 
-float bmpPression(int echelle = 100){ // Retourne la pression capté
-  return bmp.readPressure()/echelle;
+float bmpPression(int echelle = 100) { // Retourne la pression capté
+  return bmp.readPressure() / echelle;
 }
 
-float bmpAltitude(){ // Retourne l'alitude capté
+float bmpAltitude() { // Retourne l'alitude capté
   return bmp.readAltitude(SEALEVELPRESSURE_HPA);
 }
 
-String Err_bmp3xx(){ // Message d'erreur.
+String Err_bmp3xx() { // Message d'erreur.
   if (! bmp.performReading()) {
     return "Failed to perform reading :(";
   }
-  else{
+  else {
     return "BMP3XX Connected!";
   }
 }
 
-uint8_t BMP3XX_ChipID(){ // Informations sur le BMP280;
+uint8_t BMP3XX_ChipID() { // Informations sur le BMP280;
   return bmp.chipID();
 }
 
 #ifdef DEBUG // -> initialisation de l'I2C, EN MODE DEBUG SEULEMENT
-void i2c_init_(const int PIN_GPS_EN = 12,const int PIN_POW_EN = 13){
+void i2c_init_(const int PIN_GPS_EN = 12, const int PIN_POW_EN = 13) {
 
   pinMode(PIN_GPS_EN, OUTPUT);
   pinMode(PIN_POW_EN, OUTPUT);
--- cpp/StationMeteoGlacier_TEST/Test_RTC/Test_RTC.ino
+++ cpp/StationMeteoGlacier_TEST/Test_RTC/Test_RTC.ino
@@ -1,16 +1,25 @@
-#define DEBUG
-#include "RTCLibrary.h"
+/******************************************************************
+   Nom: Fichier Test pour la Librairie créer du module RTC
+   Créer par: Vincent Lavallée
+   Description: Ce programme sert de référence pour de
+                futurs utilisation de la librairies créer par les
+                stagiaires de recherche sur la station météo.
+
+******************************************************************/
+#define DEBUG //-> Definition exclusivement pour le débogage des modules individuels.
+#include "RTCLibrary.h" //Librarie développé pour le module RTC
 
 void setup() {
-  // put your setup code here, to run once:
+
   Serial.begin(115200);
-i2c_init();
-init_RTC();
-//setTimeRTC(2022,10,27,17,54,0); -> Correspond à la date de la dernière modification du temps pour la station rouge.
+  i2c_init(); //-> Initiation du I2C // fonction seulement disponible en mode DEBUG
+  init_RTC(); // Initialisation du capteur
+  
+  //setTimeRTC(2022,10,27,17,54,0); -> Correspond à la date de la dernière modification du temps pour la station rouge.
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-Serial.println(String(getTimeRTC(true)));
-delay(1000);
+  Serial.println(String(getTimeRTC(true))); //Imprime la date et l'heure enregistré par le RTC. 
+  delay(1000);
 }
