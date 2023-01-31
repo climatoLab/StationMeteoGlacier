0ef09a9d4801dc2675cf32b506c59c7046b8547a
TH0MASgit <112577645+TH0MASgit@users.noreply.github.com>
2023-01-31 10:06:45 -0500

    Déplacement du dossier de stationMeteoThingsBoard

---
+++ StationMeteoThingsBoard/FonctionsConfig.ino
@@ -0,0 +1,245 @@
+//================================================================================================================================
+//                Prise en charge de la configuration du Thingsboard via port Série. Mode "S" du menu.
+//================================================================================================================================
+bool gererSaisieCfgTB() {
+  bool configTerminee = false;
+  if (Serial.available()) {                   //Verifie si du texte a été envoyé via le moniteur série
+    String message = Serial.readStringUntil('\n');     //Mets en mémoire tout le texte envoyé par le port Série
+    if (message.charAt(0) == '#') {
+      if (message.startsWith("#TBHOST")) {
+        if (readWrite("TBHOST", message)) {
+          Serial.println("Param Thingsboard HOST sauvegardé!");
+        } else {
+          Serial.println("Erreur de param TB Host!");
+        }
+      }
+      if (message.startsWith("#TBTOKEN")) {
+        if (readWrite("TBTOKEN", message)) {
+          Serial.println("Param Thingsboard Token sauvegardé!");
+        } else {
+          Serial.println("Erreur de param TB TOKEN!");
+        }
+      }
+      if (message.startsWith("#STID")) {
+        if (readWrite("STID", message)) {
+          Serial.println("Param stationID sauvegardé!");
+        } else {
+          Serial.println("Erreur de param stationID!");
+        }
+      }
+      if (message.startsWith("#STATUS")) {
+        int indexValue = message.indexOf(':') + 1;
+        if (indexValue > 0) {
+          char etat = toupper(message.charAt(indexValue));
+          if ( etat == 'A' )
+            mesPreferences.putBool("tbStatus",true);
+          else
+            mesPreferences.putBool("tbStatus",false);
+       
+          if (mesPreferences.getBool("tbStatus"))
+            Serial.println("Param status sauvegardé: Actif");
+          else
+            Serial.println("Param status sauvegardé: Inactif");
+        }
+      }
+      if (message.startsWith("#OUBLIE")) {
+        mesPreferences.putString("TBHOST", "");
+        mesPreferences.putString("TBTOKEN", "");
+        mesPreferences.putBool("tbStatus",false);
+        Serial.println("Param TB effacés!");
+      }
+      if (message.startsWith("#END")) {
+        configTerminee = true;
+      }
+    }
+  }
+  return configTerminee;
+}
+
+
+//================================================================================================================================
+//                Prise en charge de la configuration du Wifi via port Série. Mode "W" du menu.
+//================================================================================================================================
+bool gererSaisieCfgWifi() {
+  bool configTerminee = false;
+  if (Serial.available()) {                   //Verifie si du texte a été envoyé via le moniteur série
+    String message = Serial.readStringUntil('\n');     //Mets en mémoire tout le texte envoyé par le port Série
+    if (message.charAt(0) == '#') {
+      if (message.startsWith("#SSID")) {
+        if (readWrite("SSID", message)) {
+          Serial.println("Param Wifi SSID sauvegardé!");
+        } else {
+          Serial.println("Erreur de param Wifi SSID!");
+        }
+      }
+      if (message.startsWith("#PWD")) {
+        if (readWrite("PASSWORD", message)) {
+          Serial.println("Param Wifi PWD sauvegardé!");
+        } else {
+          Serial.println("Erreur de param Wifi PWD!");
+        }
+      }
+      if (message.startsWith("#OUBLIE")) {
+        mesPreferences.putString("SSID", "");
+        mesPreferences.putString("PASSWORD", "");
+        Serial.println("Param Wifi effacés!");
+      }
+      if (message.startsWith("#END")) {
+        configTerminee = true;
+      }
+    }    
+  }
+  return configTerminee;
+}
+
+
+//================================================================================================================================
+//                Décodage
+//================================================================================================================================
+bool readWrite(String nom, String msg) {
+  bool retCode = false;
+  int indexValue = msg.indexOf(':') + 1;
+  if (indexValue > 0) {
+    char dataPlaceHolder[STRING_LENGTH];
+    copy(indexValue, msg, dataPlaceHolder);
+    if (testerStr(nom, String(dataPlaceHolder))) {
+       mesPreferences.putString(nom.c_str(), String(dataPlaceHolder));
+      retCode = true;
+    }    
+  }
+  return retCode;
+}
+
+bool testerStr(String nom, String param) {
+  bool retCode = false;
+  if (!testChar(param)) {
+     Serial.println(nom+": Parametre invalide");
+  }
+  else {
+    Serial.println(nom+": Parametre Ok");
+    retCode = true;
+  }
+  return retCode;
+}
+
+bool testChar(String str) {
+  bool retCode = false;
+  if (str.length() > 0)
+    for (int i = 0; i < STRING_LENGTH; i++) {
+      if (str.charAt(i) == '\0') {
+        retCode = true;
+      }
+    }
+  return retCode;
+}
+
+void copy(int startIndex, String msg, char* dest) {
+  int index = 0;
+  while (index < (STRING_LENGTH-1) && msg.charAt(startIndex + index) != '\r' && msg.charAt(startIndex + index) != '\n' && (startIndex + index) < msg.length()) {
+    dest[index] = msg.charAt(startIndex + index);
+    index++;
+  }
+  while (index < STRING_LENGTH) {
+    dest[index] = '\0';
+    index++;
+  }
+}
+
+
+//================================================================================================================================
+//                WiFi
+//================================================================================================================================
+
+
+bool Wifi_begin() {
+  bool retCode = false;
+
+  String ssid = mesPreferences.getString("SSID", ""); 
+  String pwd = mesPreferences.getString("PASSWORD", "");
+
+  Serial.println("SSID : " + String(ssid) + " et PWD : " + String(pwd));
+  Serial.println("> ESP32 Client WiFi. Version du code = "); Serial.println(Version);
+ 
+  if (ssid == "") {  //Il est possible d'avoir un PWD "vide"
+    Serial.println("Wifi non configuré");
+  } else {
+    //Branchement au point d'accès WiFi
+    Serial.print("> Branchement en cours avec le réseau : "); Serial.println(ssid);
+
+    enableWiFi();
+
+    WiFi.begin(ssid.c_str(), pwd.c_str());
+  
+    int compteTour = 1;
+    while (!wifiEstConnecte() && compteTour <= 30) {
+      Serial.print('.');
+      compteTour++;
+      if (compteTour%25 == 0) Serial.print("\n");
+      gererSaisieCfgWifi();
+      etat_connection = false;
+      delay(500);
+    }
+    if (wifiEstConnecte()) { 
+      retCode = true; etat_connection = true;
+      Serial.println("Connexion établie");
+      Serial.print("\r\n> Adresse MAC : "); Serial.println(WiFi.macAddress());
+      Serial.print("> Adresse IP : "); Serial.println(WiFi.localIP());
+    } else {
+      disableWiFi();
+      Serial.println("Echec de connexion");
+    }
+
+  }
+  return retCode;
+}
+
+
+void disableWiFi(){
+    WiFi.disconnect(true);  // Disconnect from the network
+    WiFi.mode(WIFI_OFF);    // Switch WiFi off
+}
+
+
+void enableWiFi(){
+    WiFi.disconnect(false);  // Reconnect the network
+    WiFi.mode(WIFI_STA);    // Switch WiFi off
+}
+
+
+bool wifiEstConnecte() {
+  return WiFi.status() == WL_CONNECTED;
+}
+
+
+//================================================================================================================================
+//                ThingsBoard
+//================================================================================================================================
+bool connecteTB(void) {
+  // Reconnect to ThingsBoard, if needed
+  bool retCode=false;
+  if (etat_connection) {
+    if (!tb.connected()) { 
+      isSubcribed = false;
+      //Récupération des valeurs:
+      String tbhost = mesPreferences.getString("TBHOST","");
+      String tbtoken = mesPreferences.getString("TBTOKEN","");
+      if (tbhost == "" || tbtoken == ""){
+         Serial.println("Thngsboard non configuré");
+      } else {
+        // Connect to the ThingsBoard
+        Serial.print("Connecting to: ");
+        Serial.print(tbhost);
+        Serial.print(" with token ");
+        Serial.println(tbtoken);
+        if (tb.connect(tbhost.c_str(), tbtoken.c_str())) {
+          retCode=true;
+          isSubcribed=true;
+          Serial.println("Connecté au serveur ThingsBoard");
+        } else {
+          Serial.println("Échec de connexion au serveur ThingsBoard");
+        }
+      } 
+    } else retCode=true;    
+  }
+  return retCode;
+}
---
+++ StationMeteoThingsBoard/FonctionsPluviometreGirouetteAnemometre.ino
@@ -0,0 +1,173 @@
+//Pluviomètre///////////////////////////////////////////////////////////////////////////////////////////////////////
+void PCF8583_config_cntr(uint8_t i2c_addr){
+  Serial.println("> Configuration du PCF8583 en mode Compteur");
+  //Démarre le bus I2C
+  Wire.begin();
+  //Petit délai d'initialisation avant la première communication avec le PCF
+  delay(100);
+
+  //Configure le PCF en mode count pulses
+  Wire.beginTransmission(i2c_addr);
+  Wire.write(0x00);   //Sélectionne le Control and status register
+  Wire.write(0x20);   //Configure les bits 4 et 5 en mode "counter" => Bit <5..4> = 0B10
+  Wire.endTransmission(); //Communique avec le PCF à cet instant
+
+  //Lecture du registre "Control and status register" pour confirmer l'écriture
+  Wire.beginTransmission(i2c_addr);
+  Wire.write(0x00);   //Sélectionnne le Control and Status register en lecture
+  Wire.endTransmission();//Communique avec le PCF à cet instant
+
+  //Lecture d'un octet du "Control and status register"
+  Wire.requestFrom(i2c_addr,1); 
+  uint8_t pcf_read_byte = Wire.read();  //Réception de la valeur du registre de contrôle
+  if(pcf_read_byte = 0x20){
+    Serial.println("> PCF8583 configuré correctement en mode Compteur \r\n");
+  }  
+  else{
+    Serial.println("> Erreur de configuration ou de communication");
+    Serial.println("> Veuillez vérifier le câblage ou la puce PCF8583");
+    Serial.println("> Arrêt du programme.");
+    while(1);
+  }  
+}
+
+void PCF8583_read_cntr(uint8_t i2c_addr){
+uint32_t pcf_counter = 0, val_tab[3];
+  //Lecture des 6 registres de compteurs
+  for(int index = 0; index < 3; index++){
+    Wire.beginTransmission(i2c_addr);
+    Wire.write(index+1); 
+    Wire.endTransmission();
+    Wire.requestFrom(i2c_addr,1); 
+    val_tab[index] = Wire.read();
+  }
+  pcf_counter = ((val_tab[2])<<16) + ((val_tab[1])<<8) + val_tab[0];
+  CompteurPluie = pcf_counter;
+  Serial.print("> Valeur du compteur = "); Serial.println(pcf_counter);
+  Serial.println("");
+}
+
+void PCF8583_clr_cntr(uint8_t i2c_addr){
+  for(uint8_t index = 0; index < 3; index++){
+    Wire.beginTransmission(PCF_I2C_ADDR);
+    Wire.write(index+1);   //Sélectionne le registre du compteur 0, 1 et 2 dans l'ordre
+    Wire.write(0x00);   //Écrit la vaeur 0x00 dans chaque registre
+    Wire.endTransmission(); //Communique avec le PCF à cet instant
+  }
+  Serial.println("> Compteur du RTC8583 effaçé correctement.");
+  Serial.println("");
+}
+
+
+//Gyrouette/////////////////////////////////////////////////////////////////////////////////////////////////////
+void girouette(){
+  if (millis() - derniereMESURECAPTEURS > delai_capteur_lecture) {
+    derniereMESURECAPTEURS = millis();
+
+    valpot = analogRead(34);
+    Serial.println("sortie analogique de la girouette = ");
+    Pcardinaux = valpot;
+    Serial.println(String(Pcardinaux) + "\n");
+
+    roseDESvents(valpot);
+  }  
+}
+
+void envoieGirouette(){
+  if (millis() - derniereEnvoieMESURECAPTEURS > delai_capteur_lecture) {
+    derniereEnvoieMESURECAPTEURS = millis();
+
+    valpot = analogRead(34);
+    Serial.println("sortie analogique de la girouette = ");
+    Pcardinaux = valpot;
+    Serial.println(String(Pcardinaux) + "\n");
+
+    roseDESvents(valpot);
+    tb.sendTelemetryFloat("Val potentiomètre", valpot);
+    tb.sendTelemetryFloat("Direction", Pcardinaux);
+  }  
+}
+
+void roseDESvents(int valpot) {
+
+ static uint16_t angle = 0;
+  if (valpot <= 715) angle = 113;       //EST SUD EST (ESE)
+  else if (valpot <= 754) angle = 68;   //EST NORD EST (ENE)
+  else if (valpot<= 814) angle = 90;   //EST (E)
+  else if (valpot <= 950) angle = 158;  //SUD SUD EST (SSE) 
+  else if (valpot <= 1123) angle = 135;  //SUD EST (SE) 
+  else if (valpot <= 1271) angle = 203;  //SUD SUD OUEST (SSO) 
+  else if (valpot<= 1522) angle = 180;  //SUD (S) 
+  else if (valpot <= 1794) angle = 23;   //NORD NORD EST (NNE)
+  else if (valpot<= 2106 ) angle = 45;   //NORD EST (NE) 
+  else if (valpot <= 2384) angle = 248;  //OUEST SUD OUEST (OSO)
+  else if (valpot <= 2563) angle = 225;  //SUD OUEST (SO)
+  else if (valpot <= 2863) angle = 338;  //NORD NORD OUEST (NNO)
+  else if (valpot <= 3094) angle = 0;    //NORD (N)
+  else if (valpot <= 3331) angle = 293;  //OUEST NORD OUEST (ONO)
+  else if (valpot <= 3664) angle = 315;  //NORD OUEST (NO)
+  else if (valpot <= 3972) angle = 270;  //OUEST (0)
+  else angle = 0;
+//  ptr->angleVent = angle;
+
+  String dirVent;
+  if(angle == 338 || angle == 0)  dirVent = "NORD";
+  else if(angle == 23 || angle == 45)   dirVent = "NORD-EST";
+  else if(angle == 68 || angle == 90)   dirVent = "EST";
+  else if(angle == 113 || angle == 135) dirVent = "SUD-EST";
+  else if(angle == 158 || angle == 180) dirVent = "SUD";
+  else if(angle == 203 || angle == 225) dirVent = "SUD-OUEST";
+  else if(angle == 248 || angle == 270) dirVent = "OUEST";
+  else if(angle == 293 || angle == 315) dirVent = "NORD-OUEST";      
+  else  dirVent = "NORD";
+
+//  if(angle == 0)  dirVent = "NORD";
+//  else if(angle == 23)  dirVent = "NNE";
+//  else if(angle == 45)  dirVent = "NORD-EST";
+//  else if(angle == 68)  dirVent = "ENE";
+//  else if(angle == 90)  dirVent = "EST";
+//  else if(angle == 113) dirVent = "ESE";
+//  else if(angle == 135) dirVent = "SUD-EST";
+//  else if(angle == 158) dirVent = "SSE";      
+//  else if(angle == 180) dirVent = "SUD";
+//  else if(angle == 203) dirVent = "SSO";  
+//  else if(angle == 225) dirVent = "SUD-OUEST";
+//  else if(angle == 248) dirVent = "OSO";  
+//  else if(angle == 270) dirVent = "OUEST";
+//  else if(angle == 293) dirVent = "ONO";
+//  else if(angle == 315) dirVent = "NORD-OUEST";
+//  else if(angle == 338) dirVent = "NNO";      
+//  else  dirVent = "N";
+
+Serial.println(dirVent);
+/*
+  
+  if (valpot >= 1550 && valpot < 2550) {
+    Serial.println("NE");
+  }
+  else if (valpot >= 2550 && valpot < 3050) {
+    Serial.println("E");
+  }
+  else if (valpot >= 3050 && valpot < 3320) {
+    Serial.println("SE");
+  }
+  else if (valpot >= 3320 && valpot < 3495) {
+    Serial.println("S");
+  }
+  else if (valpot >= 3495 && valpot < 3605) {
+    Serial.println("SO");
+  }
+  else if (valpot >= 3605 && valpot < 3700) {
+    Serial.println("O");
+  }
+  else if (valpot >= 3700 && valpot < 3760) {
+    Serial.println("NO");
+  }
+  else {
+    Serial.println("N");
+  }
+*/
+}
+
+
+//Gyrouette/////////////////////////////////////////////////////////////////////////////////////////////////////
